f7f1291045319937dfbba59446533f11cf53bc47
tinhyeunhobe1-eng <tinhyeunhobe1@gmail.com>
2026-09-14 09:18:30 -0700

    Update udid-data.js

--- udid-data.js
+++ udid-data.js
@@ -1,4 +1,1 @@
-const UDID_DATA = {
-  // Thêm khách mới theo mẫu: "UDID-của-khách": "link-file-ipa",
-  "12345abc-abcdefghijklmnop1234": "https://link-to-file.ipa",
-};
+"UDID-của-khách": "link-file-ipa-của-họ",
